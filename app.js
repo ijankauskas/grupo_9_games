16315573c818566bd884c385a6b0d5094b1e16cc
ijankauskas <ignacio.jankauskas60@gmail.com>
2022-05-26 19:10:25 -0300

    se agregan mas juegos y se empieza con login.html

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const path = require('path');
 
 app.use(express.static('public'))
 
+app.get('/login',(req, res)=>{
+    res.sendFile(path.join(__dirname,'./view/login.html'))
+})
+
+
 app.get('/',(req, res)=>{
     res.sendFile(path.join(__dirname,'./view/home.html'))
 })
