2cc345926759cb1685e62890ab25581b7356c156
Kevin Breit <kbreit32@gmail.com>
2022-05-30 20:52:11 -0300

    Creación de formulario.html, detalle del producto.html, modificación en app.js y styles.css

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ app.get('/login',(req, res)=>{
     res.sendFile(path.join(__dirname,'./view/login.html'))
 })
 
+app.get('/formulario_registro',(req, res)=>{
+    res.sendFile(path.join(__dirname,'./view/formulario_registro.html'))
+})
 
+app.get('/detalle_producto',(req, res)=>{
+    res.sendFile(path.join(__dirname,'./view/detalle_producto.html'))
+})
 app.get('/',(req, res)=>{
     res.sendFile(path.join(__dirname,'./view/home.html'))
 })
