cff340e2e9c72ea119d316001151f43075f9d35d
ijankauskas <ignacio.jankauskas60@gmail.com>
2022-08-12 11:20:53 -0300

    validaciones en el create de prodtucs

--- src/middleware/validProductsMiddleware.js
+++ src/middleware/validProductsMiddleware.js
@@ -10,6 +10,17 @@ const validation = [
     body('descripcion').notEmpty().withMessage('Tiene que tener una descripcion'),
 
     body('genero').notEmpty().withMessage('Tienes que elegir un genero'),
+
+    body('categoria').notEmpty().withMessage('Tienes que elegir un categoria'),
+
+    body('os').notEmpty().withMessage('Tienes que poner el sistema operativo minimo'),
+
+    body('procesador').notEmpty().withMessage('Tienes que poner el procesador minimo'),
+
+    body('graficos').notEmpty().withMessage('Tienes que poner los graficos minimo'),
+
+    body('almacenamiento').notEmpty().withMessage('Tienes que poner el almacenamiento minimo'),
+
 ];
 
 module.exports = validation ;
