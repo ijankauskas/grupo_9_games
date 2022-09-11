bfc6acaab0d25a47c92a910d9d5019236cedc31b
Kevin Breit <kbreit32@gmail.com>
2022-09-11 20:54:59 -0300

    Se crea archivo validators y se avanza en validaciones por front-end

---
+++ src/middleware/validators.js
@@ -0,0 +1,42 @@
+<body>
+    <main>
+        <form action="" class="formulario" id="formulario">
+
+
+            <div class="formulario_grupo" id="grupo_usuario">
+                <label for="usuario" class="formulario_label">Usuario</label>
+                <div class="formulario_grupo_input">
+                    <input type="text" class="formulario_input" name="usuario" id="usuario" placeholder="john123"></input>
+                    <i class="formulario_validacion_estado fa-regular fa-circle-xmark"></i>
+
+                </div>
+                <p class="formulario_input_error">El nombre no puede contener números</p>
+            </div>
+
+
+            <div class="formulario_grupo" id="grupo_nombre">
+                <label for="nombre" class="formulario_label">Nombre</label>
+                <div class="formulario_grupo_input">
+                    <input type="text" class="formulario_input" name="nombre" id="nombre" placeholder="john"></input>
+                    <i class="formulario_validacion_estado fa-regular fa-circle-xmark"></i>
+
+                </div>
+                <p class="formulario_input_error">El usuario tiene que ser de 4 a 16 dígitos y solo puede contener números y letras</p>
+            </div>
+
+
+            <div class="formulario_grupo" id="grupo_password">
+                <label for="password" class="formulario_label">password</label>
+                <div class="formulario_grupo_input">
+                    <input type="text" class="formulario_input" name="password" id="password" placeholder="john"></input>
+                    <i class="formulario_validacion_estado fa-regular fa-circle-xmark"></i>
+
+                </div>
+                <p class="formulario_input_error">El usuario tiene que ser de 4 a 16 dígitos y solo puede contener números y letras</p>
+            </div>
+        </form>
+    </main>
+
+<script src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"></script>
+</body>
+
