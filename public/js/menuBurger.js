b2be15021f1ec6b6cab8763c95f7ed6ef841afef
Kevin Breit <kbreit32@gmail.com>
2022-09-26 19:52:51 -0300

    Seguimos trabajando en el boton hamburguesa

--- public/js/menuBurger.js
+++ public/js/menuBurger.js
@@ -1,10 +1,21 @@
-const burger = document.querySelector(".burger-menu");
-const menu = document.querySelector("#menu");
+const e = require("express");
 
-burger.addEventListener("click", function(e){
-     menu.classList.toggle ("mostrar")
-});
+const iconoMenu = document.querySelector("#icono-menu"),
+     menu = document.querySelector("#menu");
+
+icono-menu.addEventListener("click", (e) => {
+
+// Alternamos estilos para el menu y body
+    menu.classList.toggle("active");
+    document.body.classList.toggle("opacity");
+
     
-menu.addEventListener("mouseleave", ()=>{
-    menu.classList.toggle("mostrar")
+// Alternamos su tributo "src" para el ícono del menú
+    const rutaActual = e.target.getAttribute("src");
+
+    if(rutaActual == "img/open-menu.png"){
+        e.target.setAttribute("src","img/open-menu2.png");
+    }else{
+        e.target.setAttribute("src","img/open-menu.png");
+    }
 });
